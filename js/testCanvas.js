4f38b228e9e152254bfd841ccdeba16be0646c39
Johan Taube <jtaube@student.chalmers.se>
2021-09-24 15:31:44 +0200

    Smaller color enhancements

--- js/testCanvas.js
+++ js/testCanvas.js
@@ -1,13 +1,14 @@
+var backgroundColor = 'rgb(28, 28, 28)';
 var c = document.getElementById("randomWalkCanvas");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "white";
+ctx.fillStyle = backgroundColor;
 ctx.fillRect(0, 0, c.width, c.height);
+
 
 var posX = 0;
 var posY = 0;
 var running = false;
-
-
+var currentHue = 180;
 
 function clickFunction(e) {
     if(!running){
@@ -16,8 +17,9 @@ function clickFunction(e) {
         clearRandomWalkCanvas();    
         
         var coordHolder = getMousePos(c,e);
-        ctx.fillStyle = "#FF0000";
+        ctx.fillStyle = "red";
         ctx.fillRect(coordHolder.x-5, coordHolder.y-5, 10, 10);
+        ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.lineTo(coordHolder.x, coordHolder.y);
         ctx.stroke();
@@ -35,19 +37,26 @@ function clickFunction(e) {
         }, 10);
 
         var takeStep = function(){
+            ctx.beginPath();
+            ctx.lineTo(posX, posY);
             var angle = randomAngle();
             var tempX = posX + Math.cos(angle)*10;
             var tempY = posY + Math.sin(angle)*10;
         
             ctx.lineTo(tempX, tempY);
+
+            changespeed = 10
+            ctx.strokeStyle = nextRanbowColor();//'rgb('+(steps*changespeed)%255+', '+(125+(steps*changespeed))%255+', '+(225+(steps*changespeed))%255+')';
+            ctx.closePath();
             ctx.stroke();
             steps++;
         
             if(tempX < 0 || tempX > c.width || tempY < 0 || tempY > c.height){
                 clearInterval(randomWalkAnimation);
                 running = false;
-                alert("It took " + steps + " steps to get to the edge! That is " + steps * 10 + " pixels, or " + (steps*10)/c.width + " times the width of the canvas! \n\nAssuming the same screen as I am using, that is " + + (steps*10)/c.width * 0.18 + " meters!");
+                alert("It took " + steps + " steps to get to the edge! That is " + steps * 10 + " pixels, or " + Math.round(10*((steps*10)/c.width))/10 + " times the width of the canvas! \n\nAssuming the same screen as I am using, that is " + Math.round((steps*10)/c.width * 0.18 * 100)/100 + " meters!");
             }
+
             posX = tempX;
             posY = tempY;
         }
@@ -56,10 +65,18 @@ function clickFunction(e) {
     }
 }
 
+
+
+function nextRanbowColor(){
+    currentHue = (currentHue+0.2)%360;
+    return 'hsl('+currentHue+',100%,50%)';
+}
+
 function clearRandomWalkCanvas(){
     ctx.clearRect(0, 0, c.width, c.height);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = 'rgb(28, 28, 28)';
     ctx.fillRect(0, 0, c.width, c.height);
+    currentHue = 240;
 }
 
 
